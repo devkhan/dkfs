9adc0caf9013f655ad7dba35301419443ada26b6
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-04-10 19:41:41 +0530

    Initial commit.

---
+++ dkfs.cpp
@@ -0,0 +1,12 @@
+// dkfs.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    printf("Hello World, this binary provides high-level access methods to a dkfs drive.");
+    return 0;
+}
+
