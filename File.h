8e8baf2cc27ffa9cb2e74f6ff9cce2555a62a9a3
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-04-13 00:30:04 +0530

    Stubbed File class which holds an instance of a file.

---
+++ File.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "stdafx.h"
+
+using namespace std;
+
+class File
+{
+    string fileName;
+    USHORT size;
+    LONG32 creationTime;
+    LONG32 accessionTime;
+    LONG32 modificationTime;
+
+    byte *data;
+    
+    public:
+
+        File(void* _data);
+
+        File(string name = "", void* data = nullptr);
+
+        bool setFileName(string name);
+        
+        string getFileName();
+
+        bool setModificationTime(LONG32 time);
+
+        LONG32 getModificationTime();
+
+        bool setAccessionTime(LONG32 time);
+
+        LONG32 getAccessionTime();
+
+        bool setData(void * data);
+
+        void* getData();
+
+        byte* getSerializedFile();
+};
